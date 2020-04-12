db2327da966f048b7f9dcbd179adba9ee04d591e
Pavel Mikulas <pavel.mikulas@softvision.ai>
2020-04-12 10:36:04 +0200

    Update cags_classification

--- cags_competition/cags_classification.py
+++ cags_competition/cags_classification.py
@@ -13,6 +13,8 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras import regularizers
 from keras.callbacks import LearningRateScheduler
 from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.optimizers import Adam, SGD
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 from cags_dataset import CAGS
 import efficient_net
@@ -28,6 +30,13 @@ def lr_schedule(epoch):
     if epoch > 100:
         lrate = 0.0003
     return lrate
+
+lr = 1e-3
+epochs = 125
+batch_size = 8
+es_patience = 10
+rlrop_patience = 5
+decay_rate = 0.5
 
 if __name__ == "__main__":
     # Parse arguments
@@ -66,7 +75,7 @@ if __name__ == "__main__":
     x_train, y_train = [], []
     x_dev, y_dev = [], []
     for x in train:
-        x_train.append((x['image'] * x['mask']).numpy())
+        x_train.append((x['image']).numpy())
         y_train.append(tf.keras.utils.to_categorical(x['label'].numpy(), num_classes=len(CAGS.LABELS)))
 
     for x in dev:
@@ -81,9 +90,11 @@ if __name__ == "__main__":
     efficientnet_b0 = efficient_net.pretrained_efficientnet_b0(include_top=False)
 
     datagen = ImageDataGenerator(
-            rotation_range=20,
+            rotation_range=40,
             width_shift_range=0.2,
-            height_shift_range=0.1,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2,
             horizontal_flip=True,
             fill_mode='nearest')
 
@@ -93,35 +104,61 @@ if __name__ == "__main__":
     # TODO: Create the model and train it
     efficientnet_b0.trainable = False
 
-    outputs = layers.Flatten()(efficientnet_b0.outputs[-1])
+    outputs = layers.GlobalMaxPooling2D()(efficientnet_b0.outputs[-1])
+    outputs = layers.Dropout(0.2)(outputs)
     outputs = layers.Dense(len(CAGS.LABELS), activation='softmax')(outputs)
+
 
     model = Model(inputs=efficientnet_b0.inputs, outputs=outputs)
 
-    opt = RMSprop(lr=0.001, decay=1e-6)
+    sgd = SGD(lr=lr, momentum=0.9, nesterov= True)
+    es = EarlyStopping(monitor = 'val_loss',
+                        mode = 'min',
+                        patience = es_patience,
+                        restore_best_weights = True,
+                        verbose = 1)
+
+    rlrop = ReduceLROnPlateau(monitor = 'val_loss',
+                        mode = 'min',
+                        patience = rlrop_patience,
+                        factor = decay_rate,
+                        min_lr = 1e-6,
+                        verbose = 1)
+    #opt = RMSprop(lr=0.001, decay=1e-6)
 
     model.compile(loss='categorical_crossentropy',
-            optimizer=opt,
-            metrics=[tf.keras.metrics.MeanIoU(num_classes=len(CAGS.LABELS))])
-    model.fit_generator(datagen.flow(x_train, y_train, batch_size=args.batch_size),
-                        steps_per_epoch=x_train.shape[0] // args.batch_size,
-                        epochs=125,
-                        verbose = 1, validation_data=(x_dev, y_dev), callbacks=[LearningRateScheduler(lr_schedule)])
+            optimizer=sgd,
+            metrics=['acc'])
+    model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                        epochs=epochs,
+                        verbose = 1,
+                        validation_data=(x_dev, y_dev),
+                        callbacks=[es, rlrop])
 
     model_json = model.to_json()
     with open('model.json', 'w') as json_file:
         json_file.write(model_json)
     model.save_weights('model.h5')
 
+    #print('Loading weights...')
+    #model.load_weights('model.h5')
+    #print('Model loaded')
+
+    #print('Generating test data')
     test = cags.test.map(CAGS.parse)
     x_test = []
     for x in test:
-        x_test.append(x['image'])
+        x_test.append(x['image'].numpy())
 
     x_test = np.array(x_test)
 
+    scores = model.evaluate(x_dev, y_dev, batch_size=64, verbose=1)
+    print(f'Test result: {scores[1]*100} loss:{scores[0]}')
+
+    print('Generating predictions...')
+
     # Generate test set annotations, but in args.logdir to allow parallel execution.
-    with open(os.path.join(args.logdir, "cags_classification.txt"), "w", encoding="utf-8") as out_file:
+    with open("cags_classification.txt", "w", encoding="utf-8") as out_file:
         # TODO: Predict the probabilities on the test set
         test_probabilities = model.predict(x_test, batch_size=args.batch_size)
         for probs in test_probabilities:
